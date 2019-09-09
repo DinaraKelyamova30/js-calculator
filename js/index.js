0747bdcf04e8b183d333c4b0ff5b1a4599f5168d
DinaraKelyamova30 <kelyamova30@gmail.com>
2019-09-09 13:04:10 +0000

    mmm

--- js/index.js
+++ js/index.js
@@ -1,17 +1,38 @@
-// Calculator
-    const nums = document.querySelectorAll('.calc .nums button');
-    const opers = document.querySelectorAll('.calc .opers button');
-    const display = document.querySelector('.calc .display');
-    const equal = document.querySelector('.calc .eq');
-    nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed) );
-    function numPressed(ev) {
-        const num = ev.target.innerText;
-        display.value += num;
-    }
-    opers.forEach( buttonOper => buttonOper.addEventListener('click', operPressed) );
-    function operPressed(ev) {
-        const oper = ev.target.innerText;
-        display.value += oper;
-    }
-    equal.addEventListener('click', () => display.value = eval(display.value) );
-    })();
+const nums = document.querySelectorAll('.calc .nums button');
+const opers = document.querySelectorAll('.calc .opers button');
+const display = document.querySelector('.calc .display');
+const equal = document.querySelector('.calc .eq');
+const clear = document.querySelector(".clear");
+nums.forEach( buttonNum => buttonNum.addEventListener('click', numPressed) );
+function numPressed(ev) {
+    const num = ev.target.innerText;
+    display.value += num;
+}
+opers.forEach( buttonOper => buttonOper.addEventListener('click', operPressed) );
+function operPressed(ev) {
+    const oper = ev.target.innerText;
+    display.value += oper;
+}
+equal.addEventListener('click', () => display.value = eval(display.value) );
+
+function clearPressed(ev) {
+    const clear = ev.target.innerText;
+    display.value += clear;
+}
+clear.addEventListener = () => display.value = '';
+
+// function equalPressed(ev) {
+//     const equal = eval(equation);
+//     output.innerHTML = answer;
+// }
+// equal.addEventListener("click", equalPressed);
+// function clePressed(ev) {
+//   output.innerHTML = "";
+//   equation = "";
+//   answer = 0;
+// }
+// clear.addEventListener("click", clickClear);
+
+
+
+
